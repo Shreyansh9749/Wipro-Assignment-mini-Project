2a8f41f41f1ee275a4798f953cbf8b0fa850e6c2
Shreyansh9749 <162117622+Shreyansh9749@users.noreply.github.com>
2025-10-31 13:32:23 +0530

    Delete Spring Boot directory

--- Spring Boot/SpringBoot MVC JPA H2/Assignment01/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wipro.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.abhishek.model.Employee;
-public interface EmployeeRepository extends CrudRepository<Employee, Integer>
-{
-}
